7e28e7d074f28ea628fb968b05c0f13a2627b644
Sergei Borja <sergei.borja0107@gmail.com>
2024-09-13 17:17:12 -0600

    Fixed assets.

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import StorageMenu from "./components/StorageMenu/StorageMenu.tsx";
 import StorageMenuButtons from "./components/StorageMenu/StorageMenuButtons.tsx";
 import StorageIndicator from "./components/StorageIndicator/StorageIndicator.tsx";
+import DocumentsIcon from "/images/icon-document.svg";
+import FolderIcon from "/images/icon-folder.svg";
+import UploadIcon from "/images/icon-upload.svg";
 
 export default function App(): React.JSX.Element {
 
     return (
         <main className='storage-page'>
             <StorageMenu>
-                <StorageMenuButtons label='Documents' location="#" icon="./images/icon-document.svg"/>
-                <StorageMenuButtons label="Folder" location="#" icon="./images/icon-folder.svg"/>
-                <StorageMenuButtons label="Upload" location="#" icon="./images/icon-upload.svg"/>
+                <StorageMenuButtons label='Documents' location="#" icon={DocumentsIcon}/>
+                <StorageMenuButtons label="Folder" location="#" icon={FolderIcon}/>
+                <StorageMenuButtons label="Upload" location="#" icon={UploadIcon}/>
             </StorageMenu>
             <StorageIndicator totalStorage={1000} usedStorage={815}/>
         </main>
